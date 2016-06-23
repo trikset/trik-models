6b474716a28428f9821ad722c780f01119425e71
greg <zimin_grigory@hotmail.com>
2016-06-23 22:47:16 +0300

    script for simple starting other scripts
    usage: add s.link with name autorun.js into trik/scripts
    
    
    Signed-off-by: greg <zimin_grigory@hotmail.com>

---
+++ autostart-service/system_d_Service.js
@@ -0,0 +1,53 @@
+var powerMotorLeft  = brick.motor(M1);
+var powerMotorRight = brick.motor(M2);
+
+var scriptLines =
+	"#put all new lines here\n" 
+	+ "sleep 20\n"
+
+var scriptToKill = "autorun." + new Date().getTime() + ".sh"
+var scriptFileName = "/tmp/" + scriptToKill
+
+script.writeToFile(scriptFileName, scriptLines);
+script.system("chmod +x " + scriptFileName)
+
+gamepad.pad.connect(
+	function(padId, x, y) {
+		powerMotorLeft.setPower(y+x);
+		powerMotorRight.setPower(y-x);
+	}
+)
+
+gamepad.padUp.connect(
+	function(padId, x, y) {
+		powerMotorLeft.powerOff();
+		powerMotorRight.powerOff();
+	}
+)
+
+gamepad.button.connect(
+  function(buttonId, pressed) {
+    switch (buttonId) {
+      case 1: {
+		var command = "killall -9 -q " + scriptToKill;
+		script.system(command);
+		brick.led().orange();
+        break;
+      }
+      case 2: {
+		var command = scriptFileName
+			+ " && echo 1 > /sys/class/leds/led_red/brightness" 
+			+ " && echo 1 > /sys/class/leds/led_green/brightness";
+
+		brick.led().green();
+		script.system(command);
+        break;
+      }
+      default: break;
+    }
+  }
+)
+
+brick.led().orange();
+
+script.run();
