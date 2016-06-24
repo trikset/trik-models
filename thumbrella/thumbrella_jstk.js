e949c00c181c6e74cc1073756293f756ad22fb31
Lada Gagina <lada.gagina@gmail.com>
2016-06-24 20:02:29 +0300

    Refactored thumbrella_jstk.js

--- thumbrella/thumbrella_jstk.js
+++ thumbrella/thumbrella_jstk.js
@@ -8,58 +8,11 @@ var yaw = 0;
 var spd = 0;
 var uds = 0;
 var sqs = 0;
+var jstk = brick.eventDevice(jstk_pth);
 
-brick.eventDevice(jstk_pth).on.connect(
-	function(event_, code_, value_, eventTime_) {
-    if(event_ != 0){
-      switch(code_) {
-        case PadEventCodes.X:
-          yaw = value_*100/128;
-          break;
-        case PadEventCodes.Y:
-          spd = -1*value_*100/128;
-          break;
-        case PadEventCodes.Throttle:
-          uds = (value_-128)*100/128;
-          break;
-        case PadEventCodes.BtnY:
-          if(value_)
-            sqs = 100;
-          else
-            sqs = 0;
-          break;
-        case PadEventCodes.BtnTL:
-          if(value_)
-            sqs = -100;
-          else
-            sqs = 0;
-          break;
-          case PadEventCodes.BtnA:
-            if(value_)
-              pp(1);
-            break;
-          case PadEventCodes.BtnB:
-            if(value_)
-              pp(2);
-            break;
-          case PadEventCodes.BtnC:
-            if(value_)
-              pp(3);
-            break;
-        default:
-          print("pew");
-      }
-      
-      //print(event_, spd, yaw);
-      
-      R.setPower(spd - yaw);
-      L.setPower(spd + yaw);
-      UD.setPower(uds);
-      SQ.setPower(sqs);
-      
-    }
-  }
-)
+if (jstk != null) {
+  brick.eventDevice(jstk_pth).on.connect(jstk_handle_event);
+}
 
 gamepad.pad.connect(
 	function(padId, x, y) {
@@ -93,11 +46,61 @@ gamepad.button.connect(
 			return;
 		}
 
-		pp(buttonId);
+		handle_button_down(buttonId);
 	}
 )
 
-function pp(buttonId) {
+function jstk_handle_event(event_, code_, value_, eventTime_) {
+  if(event_ != 0){
+    switch(code_) {
+      case PadEventCodes.X:
+        yaw = value_*100/128;
+        break;
+      case PadEventCodes.Y:
+        spd = -1*value_*100/128;
+        break;
+      case PadEventCodes.Throttle:
+        uds = (value_-128)*100/128;
+        break;
+      case PadEventCodes.BtnY:
+        if(value_)
+          sqs = 100;
+        else
+          sqs = 0;
+        break;
+      case PadEventCodes.BtnTL:
+        if(value_)
+          sqs = -100;
+        else
+          sqs = 0;
+        break;
+        case PadEventCodes.BtnA:
+          if(value_)
+            handle_button_down(1);
+          break;
+        case PadEventCodes.BtnB:
+          if(value_)
+            handle_button_down(2);
+          break;
+        case PadEventCodes.BtnC:
+          if(value_)
+            handle_button_down(3);
+          break;
+      default:
+        print("Unknown button code");
+    }
+    
+    //print(event_, spd, yaw);
+    
+    R.setPower(spd - yaw);
+    L.setPower(spd + yaw);
+    UD.setPower(uds);
+    SQ.setPower(sqs);
+    
+  }
+}
+
+function handle_button_down(buttonId) {
   switch (buttonId) {
     case 1:
       brick.smile();
