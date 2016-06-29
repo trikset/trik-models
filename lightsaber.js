e316b8d6ea52af00672b76f40c65395bfeddc4a9
dmitry42nd <dmitry.42nd@gmail.com>
2016-06-29 16:42:28 +0300

    light saber light added

--- lightsaber.js
+++ lightsaber.js
@@ -1,31 +1,43 @@
-//tiny playTone syntax example
-var r = brick.motor(M1).setPower;
-var g = brick.motor(M2).setPower;
-var b = brick.motor(M3).setPower;
-var d = brick.motor(M4).setPower;
+//lightsaber
+var r = brick.motor(M3).setPower;
+var g = brick.motor(M1).setPower;
+var b = brick.motor(M4).setPower;
+var d = brick.motor(M2).setPower; //gnD
 
 var gyr = brick.gyroscope();
 var play = brick.playTone;
 
-brick.keys().buttonPressed.connect(
-  function(code, value) {
-    var freq = 0;
-    if(!value) return;
+//color flags
+var rf = false;
+var gf = false;
+var bf = false;
 
+brick.keys().buttonPressed.connect(
+  function(code, value) { //saber color sets here
+    if(value != 1) return;
     switch(code) {
-      case KeysEnum.Up  : freq = 20; break;
-      case KeysEnum.Left: freq = 30; break;
-      case KeysEnum.Down: freq = 40; break;
-      default: freq = 0;
+      case KeysEnum.Up  :
+        var rout = rf ? 100 : -100;
+        rf = !rf;
+        r(rout);
+        break;
+      case KeysEnum.Left:
+        var gout = gf ? 100 : -100;
+        gf = !gf;
+        g(gout);
+        break;
+      case KeysEnum.Down:
+        var bout = bf ? 100 : -100;
+        bf = !bf;
+        b(bout);
+      break;
+      default: break;
     }
-    print(freq);
-    if(freq)
-      brick.playTone(freq, 2000);
   }
 );
 
 var freq = 0;
-var BASE = 50; 
+var BASE = 20; 
 var gdata = [0,0,0];
 gyr.newData.connect(
   function(data_, time_) {
@@ -33,15 +45,21 @@ gyr.newData.connect(
   }
 )
 
+var sat = function (a) {
+  var max = 70
+  if (a > max)
+    return max;
+  else
+    return a;
+}
 
 var wewLoop = function() {
-  var freq_new = (Math.abs(gdata[0]) + Math.abs(gdata[1]) + Math.abs(gdata[2])) >> 10;
-  freq_new *= 5;
+  var freq_new = sat((Math.abs(gdata[0]) + Math.abs(gdata[1]) + Math.abs(gdata[2])) >> 9);
   print(freq_new);
 
-  if(freq_new <= 20) {
+  if(freq_new < 20) {
     if(freq)
-      brick.playTone(20, 10);
+      play(20, 10);
     freq = 0;
   }
   else if(freq != freq_new) {
@@ -51,9 +69,15 @@ var wewLoop = function() {
 }
 
 var main = function() {
+  d(-100); //wisely the pin you should choose
+  
+  r(100);
+  g(-100);
+  b(100);
   script.system("echo 0 > /sys/class/misc/l3g42xxd/odr_selection");
-  var mainT = script.timer(10);
-  mainT.timeout.connect(wewLoop);echo 3 > /sys/class/misc/mma845x/odr_selection
+  
+  var mainT = script.timer(100);
+  mainT.timeout.connect(wewLoop);
   mainT.stop();
   mainT.start();
   script.run();
