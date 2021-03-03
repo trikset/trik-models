c45d141f6064b34316ee785ac125a9443a3b80a0
Andrei Khodko <andrei.khodko@gmail.com>
2021-03-04 02:52:34 +0300

    Async and try to use webcam

--- lobeClient/desktopClient.py
+++ lobeClient/desktopClient.py
@@ -1,68 +1,95 @@
 """
 Read https://github.com/lobe/lobe-python#lobe-python-api for help and required dependencies
 """
-
+import asyncio
 from lobe import ImageModel
+from VideoCapture import Device
 import socket
-import time
+
 
 model = ImageModel.load('path/to/exported/model')
 
-SERVER_IP = "192.168.77.1"
+# SERVER_IP = "192.168.77.1"
+SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8889
-KEEPALIVE_TIMER = 1
+KEEPALIVE_TIMER = 5
 MY_HULL_NUMBER = 1
 
-def send_message(message, resource):
-    """
-    Sends the message to the resource in the correct format
-    :param message: Message to be send
-    :param resource: Socket to which the data will be delivered
-    """
-    byte_message = formatted_data_in_bytes(message)
-    print(byte_message)
-    resource.send(byte_message)
+GET_IMAGES_FROM_ROBOT = True
+# PHOTO_PATH = 'http://' + SERVER_IP + ':8080/?action=snapshot'
+PHOTO_PATH = 'http://192.168.77.1:8080/?action=snapshot'
 
 
-def formatted_data_in_bytes(data: str) -> bytes:
-    """
-    Reformates string data into a bytes array `<data size>:data`
-    :param data: Data to be reformatted
-    :return: Data in bytes in the required format
-    """
-    return bytes(str(len(data)) + ":" + data, encoding='UTF-8')
+async def send(message, sock):
+    try:
+        byte_message = formatted_data_in_bytes(message)
+        print("Send:", byte_message)
+        async with asyncio.Lock():
+            sock.send(byte_message)
+        await asyncio.sleep(0.2)
+    except ConnectionResetError:
+        pass
+
+
+async def send_message(message: str, sock):
+    await send('data:' + message, sock)
+
+
+def formatted_data_in_bytes(msg: str) -> bytes:
+    return bytes(str(len(msg)) + ":" + msg, encoding='UTF-8')
+
+
+def predict(path: str):
+    if GET_IMAGES_FROM_ROBOT:
+        return model.predict_from_url(path).prediction
+    else:
+        return model.predict(Device().getImage()).prediction
+
+
+async def send_keepalive(sock):
+    while True:
+        await send('keepalive', sock)
+        await asyncio.sleep(KEEPALIVE_TIMER)
+
+
+async def send_prediction(sock):
+    while True:
+        await send_message(predict(PHOTO_PATH), sock)
+
+
+async def read_data(sock, my_loop):
+    data = ""
+    while data != "9:data:quit":
+        await asyncio.sleep(0.2)
+        try:
+            data = (await my_loop.sock_recv(sock, 255)).decode("utf-8")
+        except ConnectionResetError:
+            pass
+
+        if data:
+            print("Received:", data)
+
+    sock.close()
+    my_loop.stop()
 
 
 if __name__ == '__main__':
-    server = socket.socket()
-    server.connect((SERVER_IP, SERVER_PORT))
-
-    ip, port = server.getsockname()
-    send_message('register:{}:{}'.format(ip, port), server)
-    send_message('self:{}'.format(MY_HULL_NUMBER), server)
-
-    photo_url = 'http://' + SERVER_IP + ':8080/?action=snapshot'
-
     try:
-        while response != "quit":
-            result = model.predict_from_url(photo_url)
-            send_message('data:{}'.format(result.prediction), server)
+        loop = asyncio.get_event_loop()
 
-            data = ""
-            try:
-                data = resource.recv(1024)
-            except ConnectionResetError:
-                pass
+        server = socket.socket()
+        server.connect((SERVER_IP, SERVER_PORT))
 
-            if data:
-                print(data)
+        ip, port = server.getsockname()
+        asyncio.ensure_future(send('register:{}:{}'.format(port, MY_HULL_NUMBER), server))
+        asyncio.ensure_future(send('self:{}'.format(MY_HULL_NUMBER), server))
 
-            time.sleep(KEEPALIVE_TIMER)
+        asyncio.ensure_future(send_keepalive(server))
+        asyncio.ensure_future(send_prediction(server))
+        asyncio.ensure_future(read_data(server, loop))
 
-        server.close()
-        print("Connection was closed.")
+        loop.run_forever()
     except KeyboardInterrupt:
-        server.close()
         print("Connection was closed.")
-    except:
-        print("Connection was terminated unexpectedly.")
+        server.close()
+        loop.stop()
