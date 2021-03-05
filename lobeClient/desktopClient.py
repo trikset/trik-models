b6b8f5efaecba655718368d3f15df9330ac48850
Andrei Khodko <andrei.khodko@gmail.com>
2021-03-05 03:28:24 +0300

    Guide and auto pckg install

--- lobeClient/desktopClient.py
+++ lobeClient/desktopClient.py
@@ -1,25 +1,45 @@
 """
 Read https://github.com/lobe/lobe-python#lobe-python-api for help and required dependencies
 """
+import subprocess
+import sys
 import asyncio
-from lobe import ImageModel
-import cv2
 import socket
 
-
-model = ImageModel.load('path/to/exported/model')
-
-# SERVER_IP = "192.168.77.1"
-SERVER_IP = "127.0.0.1"
+# IP робота или студии. Для студии обычно использовать SERVER_IP = '127.0.0.1'
+SERVER_IP = '192.168.77.1'
 SERVER_PORT = 8889
 KEEPALIVE_TIMER = 5
 MY_HULL_NUMBER = 1
 
-GET_IMAGES_FROM_ROBOT = False
-# PHOTO_PATH = 'http://' + SERVER_IP + ':8080/?action=snapshot'
-PHOTO_PATH = 'http://192.168.77.1:8080/?action=snapshot'
+# Путь к директории обученной модели Lobe. Работает с TFLite
+MODEL_PATH = 'path/to/exported/model'
 
-CAMERA = cv2.VideoCapture(0)
+# Установить True если хотим использовать изображение с камеры ТРИК (нужно запустить mjpg-encoder)
+# Установить False если хотим использовать изображение с вебкамеры компьютера
+GET_IMAGES_FROM_ROBOT = False
+
+# Номер камеры в ОС Windows. Разные значения активирует разные подключенные камеры (0, 1, 2...)
+CAMERA_NUMBER = 0
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+try:
+    import cv2
+    from lobe import ImageModel
+except ImportError:
+    install('opencv-python')
+    install('git+https://github.com/lobe/lobe-python')
+    import cv2
+    from lobe import ImageModel
+
+
+model = ImageModel.load(MODEL_PATH)
+CAMERA = cv2.VideoCapture(CAMERA_NUMBER)
+PHOTO_PATH = 'http://' + SERVER_IP + ':8080/?action=snapshot'
 
 
 async def send(message, sock):
